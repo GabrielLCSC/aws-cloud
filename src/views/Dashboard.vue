9b59affc9f0136091c12ed0d985b50a953b3e966
Hugo <macedohugo77@gmail.com>
2025-04-14 14:52:13 +0200

    first commit

---
+++ src/views/Dashboard.vue
@@ -0,0 +1,20 @@
+<template>
+    <div class="container mt-5">
+      <h2>Bienvenue sur ton tableau de bord</h2>
+      <button @click="logout" class="btn btn-danger mt-3">Se déconnecter</button>
+    </div>
+  </template>
+  
+  <script>
+  import auth from '../store/auth';
+  
+  export default {
+    methods: {
+      logout() {
+        auth.logout();
+        this.$router.push('/login');
+      }
+    }
+  };
+  </script>
+  
