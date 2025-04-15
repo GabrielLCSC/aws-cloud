fc2e087fe942692f8d4a468c174d59c0592de035
BigZoo92 <givernaudenzo@gmail.com>
2025-04-15 10:42:39 +0200

    wip

--- src/amplifyConfig.js
+++
@@ -1,5 +0,0 @@
-import { Amplify } from '@aws-amplify/core'
-import amplifyconfig from './amplifyconfiguration.json'
-
-Amplify.configure(amplifyconfig)
-
--- src/main.js
+++ src/main.js
@@ -1,10 +1,13 @@
-import './amplifyConfig'
+import { Amplify } from 'aws-amplify';
+import amplifyconfig from './amplifyconfiguration.json';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './assets/main.css'
 import { createApp } from 'vue';
 import App from './App.vue';
 import router from './router';
 import Antd from 'ant-design-vue'
+
+Amplify.configure(amplifyconfig);
 
 const app = createApp(App)
 
